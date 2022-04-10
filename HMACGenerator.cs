26ebdd99496dc0e274698fc99f260fc0ee2083d7
Vladislav Podymov <vladpod36564@gmail.com>
2022-04-10 16:10:53 +0300

    Add project files.

---
+++ HMACGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace task3_kamenoznicy
+{
+    class HMACGenerator
+    {
+        public int SelectedMove { get; set; }
+        public string HMACValue { get; set; }
+        public string HMACKey { get; set; }
+
+        public HMACGenerator(int argsCount)
+        {
+            CreateKey(argsCount);
+            SelectedMove = GetRandomMoveNumber(argsCount);
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            HMACSHA256 hmacsha256 = new HMACSHA256(encoding.GetBytes(HMACKey));
+            HMACValue = ByteToString(hmacsha256.ComputeHash(encoding.GetBytes((SelectedMove + 1).ToString())));
+
+        }
+        private static int GetRandomMoveNumber(int maxValue)
+        {
+            var number = RandomNumberGenerator.GetInt32(maxValue);
+            return number;
+        }
+
+        private static int GetRandomNumber()
+        {
+            var number = RandomNumberGenerator.GetInt32(Int32.MaxValue);
+            return number;
+        }
+
+        private void CreateKey(int argsCount)
+        {
+            HMACKey = "";
+            for (int i = 0; i < argsCount; i++)
+            {
+                HMACKey += GetRandomNumber().ToString("X");
+            }
+        }
+
+        private static string ByteToString(byte[] buff)
+        {
+            string sbinary = "";
+            for (int i = 0; i < buff.Length; i++)
+            {
+                sbinary += buff[i].ToString("X2");
+            }
+            return (sbinary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿namespace task3_kamenoznicy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CheckArgs(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            HMACGenerator computerMove = new(args.Length);
+            while (true)
+            {
+                Console.WriteLine($"HMAC: {computerMove.HMACValue}");
+                Console.WriteLine("Moves menu:");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {args[i]}");
+                }
+                Console.WriteLine("0. exit\n?. help");
+                string input = GetValidInput(args.Count());
+                switch (input)
+                {
+                    case "0":
+                        {
+                            Environment.Exit(0);
+                            break;    
+                        }
+                    case "?":
+                        {
+                            Table table = new(args);
+                            Console.WriteLine(table.ToString());
+                            break;
+                        }
+                    default:
+                        {
+                            int move = int.Parse(input)-1;
+                            Console.WriteLine($"Your move: {args[move]}");
+                            Console.WriteLine($"Computer move: {args[computerMove.SelectedMove]}");
+                            Console.WriteLine(GetResult(move, computerMove.SelectedMove, args.Length));
+                            Console.WriteLine($"HMAC key: {computerMove.HMACKey}");
+                            computerMove = new(args.Length);
+                            break;
+                        }
+                }
+                Console.WriteLine("/////");
+            }
+
+        }
+
+        static string GetResult(int userMove, int computerMove, int argsCount)
+        {
+            if (userMove == computerMove)
+            {
+                return "Tie!";
+            }
+            else
+            {
+                string row = Table.GetStartString(argsCount);
+                while (row[userMove] != '0')
+                {
+                    row = Table.ShiftString(row);
+                }
+                if (row[computerMove] == '1')
+                {
+                    return "You win!";
+                }
+                else
+                {
+                    return "You lose!";
+                }
+            }
+        }
+
+        static string GetValidInput(int argsCount)
+        {
+            string input = "";
+            Console.WriteLine("Enter your move:");
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "?") break;
+                else if (int.TryParse(input, out int result) && result >= 0 && result <= argsCount) break;
+                else Console.WriteLine("Invalid input. Enter your move:");
+            }
+
+            return input;
+        }
+
+        static void CheckArgs(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                throw new Exception("Error: Invalid amount of arguments, argument amount must be an odd value larger than 2. ");
+            }
+            else if (args.Length % 2 == 0)
+            {
+                throw new Exception("Error: Amount of arguments must be odd.");
+            }
+            else if (!isDistinct(args))
+            {
+                throw new Exception("Error: Input must not contain duplicate arguments.");
+            }
+        }
+
+        static bool isDistinct(string[] arr)
+        {
+            HashSet<string> s = new(arr);
+            return (s.Count == arr.Length);
+        }
+    }
+}
